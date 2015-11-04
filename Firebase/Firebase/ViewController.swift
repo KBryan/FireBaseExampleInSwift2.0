e4aa7bbe0f1db6d6df4cc89cd3133de51c70d12b
KBryan <kwame@3einteractive.com>
2015-11-04 05:39:07 -0500

    Starter to Firebase and Swift Tutorial

---
+++ Firebase/Firebase/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by KBryan on 2015-11-03.
+//  Copyright © 2015 KBryan. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var myRootRef = Firebase(url:"")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
